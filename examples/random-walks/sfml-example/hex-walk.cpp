65527008e3e01fca262dad7b0ca57c862f748379
Josie Thompson <dots.slash.josie@gmail.com>
2024-03-12 14:20:15 -0700

    added example for random hex walk

--- examples/random-walks/sfml-example/hex-walk.cpp
+++ examples/random-walks/sfml-example/hex-walk.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <random>
+
 #include <array>
+#include <unordered_set>
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "tess/tess.hpp"
+#include "spatula/directions.hpp"
+#include "simulacrum/random_walks.hpp"
+
+namespace sim = simulacrum;
 
 struct app_settings{
     std::string window_name{ "hex-walk" };
     sf::VideoMode video_mode{ 800, 600 };
     sf::Color bg_color = sf::Color::White;
 
-    float unit_size = 50.f;
+    float unit_size = 10.f;
+    std::uint32_t num_steps = 200;
 };
 
 // convert a hex coordinate to an sfml shape
@@ -42,16 +50,32 @@ int main()
                                       static_cast<float>(settings.video_mode.height)/2.f,
                                       settings.unit_size };
 
+    std::random_device seed{};
+    std::mt19937 rng{seed()};
+
     // initialize the hexes we're working with
     // and set some basic graphical settings
     std::vector<tess::hex<int>> hexes;
-    tess::hex_range(tess::hex<int>::zero, 3, std::back_inserter(hexes));
+    hexes.reserve(settings.num_steps);
+    auto into_hexes = std::inserter(hexes, hexes.begin());
+
+    using pointed_hex = sp::pointed_hex::direction_name;
+    const tess::hex<int> start{ 0, 0 };
+    std::generate_n(into_hexes, settings.num_steps,
+                    sim::uniform_walk<pointed_hex>(rng, start));
 
     std::vector<sf::ConvexShape> shapes;
     shapes.reserve(hexes.size());
     for (const auto& hex : hexes) {
         auto& shape = shapes.emplace_back(hex_shape(basis, hex));
-        shape.setFillColor(sf::Color::Black);
+        std::cout << "(" << hex.q << ", " << hex.r << ")\n";
+
+        if (hex == tess::hex<int>::zero) {
+            shape.setFillColor(sf::Color::Cyan);
+        }
+        else {
+            shape.setFillColor(sf::Color::Black);
+        }
         shape.setOutlineColor(sf::Color::White);
         shape.setOutlineThickness(1.0f);
     }
